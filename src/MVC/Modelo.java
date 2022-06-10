59f6013d95c17614085161930be90d9de8ae5bee
Martín Cardona <martincardonademiguel@gmail.com>
2022-06-10 22:57:18 +0200

    Añadido cargarTabla
    (Probado en otro proyecto y funcionando, en este no podemos ya que
    tenemos un problema con la conexión SQL)

--- src/MVC/Modelo.java
+++ src/MVC/Modelo.java
@@ -1,9 +1,11 @@
 package MVC;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.sql.*;
@@ -34,7 +36,7 @@ public class Modelo {
 	private String pwd = "";
 	private String url = "jdbc:mysql://localhost/" + bd;
 	private Connection conexion;
-	
+
 	private PantallaAdmin admin;
 	private Config config;
 	private CreateEventWindow createEvents;
@@ -59,86 +61,83 @@ public class Modelo {
 	private String sqlTablaUsuario = "Select * from usuario";
 	private DefaultTableModel table1;
 	private DefaultTableModel table2;
-	
-	
+
 	private Vista vista;
-	
-	private String usr= "";
-	private String pass= "";
-	private String rol= "";
-	
+
+	private String usr = "";
+	private String pass = "";
+	private String rol = "";
+
 	public void setPwdBBDD(String usuario) {
-		this.pwd=usuario;
+		this.pwd = usuario;
 	}
-	
+
 	public String getPwdBBDD() {
 		return this.pwd;
 	}
+
 	public void setUsrBBDD(String usuario) {
-		this.login=usuario;
+		this.login = usuario;
 	}
-	
+
 	public String getUsrBBDD() {
 		return this.login;
 	}
-	
+
 	public void setNameBBDD(String usuario) {
-		this.bd=usuario;
+		this.bd = usuario;
 	}
-	
+
 	public String getNameBBDD() {
 		return this.bd;
 	}
-	
 
-	
 	public void setVista(Vista vista) {
 		this.vista = vista;
 	}
-	
+
 	public Modelo() {
 		propconfig = new Properties();
 		try {
-			
+
 			fichero = new File(ficheroConf);
 			if (fichero.exists()) {
-				
+
 				entrada = new FileInputStream(fichero);
 				propconfig.load(entrada);
-				
+
 			} else {
-				
+
 				System.err.println("No encontrado");
-				
+
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-    }
-	
+
+	}
+
 	public void ConexSQL() {
 		try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conexion = DriverManager.getConnection(propconfig.getProperty("bd"), propconfig.getProperty("login"), propconfig.getProperty("pwd"));
-            System.out.println(" - Conexion con MySQL establecida -");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conexion = DriverManager.getConnection(propconfig.getProperty("bd"), propconfig.getProperty("login"),
+					propconfig.getProperty("pwd"));
+			System.out.println(" - Conexion con MySQL establecida -");
 //            if (conexion != null) {
-            // System.out.println("Conexion a la bd" + url + "...ok!!!");
-        } catch (ClassNotFoundException cnfe) {
-            System.out.println("Driver JDBC no encontrado");
-            cnfe.printStackTrace();
-        } catch (SQLException sqle) {
-            System.out.println("Error al conectarte a la DB");
-            sqle.printStackTrace();
-        } catch (Exception e) {
-            System.out.println("- Error de Conexion con MySQL -");
-            e.printStackTrace();
-        }
+			// System.out.println("Conexion a la bd" + url + "...ok!!!");
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println("Driver JDBC no encontrado");
+			cnfe.printStackTrace();
+		} catch (SQLException sqle) {
+			System.out.println("Error al conectarte a la DB");
+			sqle.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("- Error de Conexion con MySQL -");
+			e.printStackTrace();
+		}
 	}
-	
+
 	public String LoginSQL(String query, String usr, String NumColumn) {
 		ResultSet resultado = null;
 		String resul = "";
@@ -153,34 +152,32 @@ public class Modelo {
 		}
 		return resul;
 	}
-	
-	
+
 	public boolean comprobarContra(String usuario, String contrasena) {
-		this.pass = LoginSQL("SELECT contrasena FROM Usuario WHERE NombreUsuario = ?",usuario,"NombreUsuario");
-		
-		if(contrasena.equals(pass)) {
-			return true;	
-		}else {
-		return false;
+		this.pass = LoginSQL("SELECT contrasena FROM Usuario WHERE NombreUsuario = ?", usuario, "NombreUsuario");
+
+		if (contrasena.equals(pass)) {
+			return true;
+		} else {
+			return false;
 		}
 	}
-	
-	//public int getPlayers(String NameEvent) {
-		//int players = 0;
-		//ResultSet resultado = null;
-		//try {
-			//PreparedStatement pstmt = conexion.prepareStatement("SELECT Players FROM Events WHERE EventName = ?");
-			//pstmt.setInt(1, 2);
-			//resultado = pstmt.executeQuery();
-			//players = resultado.getInt(2);
-			//pstmt.close();
-		//} catch (SQLException e) {
-			//System.err.println(e.getMessage());
-		//}
-		//return players;
-	//}
-	
-	
+
+	// public int getPlayers(String NameEvent) {
+	// int players = 0;
+	// ResultSet resultado = null;
+	// try {
+	// PreparedStatement pstmt = conexion.prepareStatement("SELECT Players FROM
+	// Events WHERE EventName = ?");
+	// pstmt.setInt(1, 2);
+	// resultado = pstmt.executeQuery();
+	// players = resultado.getInt(2);
+	// pstmt.close();
+	// } catch (SQLException e) {
+	// System.err.println(e.getMessage());
+	// }
+	// return players;
+	// }
 
 	public void setAdmin(PantallaAdmin admin) {
 		this.admin = admin;
@@ -241,22 +238,22 @@ public class Modelo {
 	public void setPropconfig(Properties propconfig) {
 		this.propconfig = propconfig;
 	}
-	
+
 	public void guardar(String[] datosConexion, String[] keys) {
 		try {
-			
-			for(int i = 0; i < keys.length; i++) {
+
+			for (int i = 0; i < keys.length; i++) {
 				propconfig.setProperty(keys[i], datosConexion[i]);
 				salida = new FileOutputStream(fichero);
 				propconfig.store(salida, "Guardado");
 				respuesta = "Guardado";
 			}
-			
-		}catch(Exception e){
-			
+
+		} catch (Exception e) {
+
 		}
 	}
-	
+
 	public void cargarTablaEventos() {
 		table1 = new DefaultTableModel();
 		int numColumnas = getNumColumnas(sqlTablaEventos);
@@ -267,7 +264,7 @@ public class Modelo {
 			ResultSet rset = pstmt.executeQuery();
 			ResultSetMetaData rsmd = rset.getMetaData();
 			for (int i = 0; i < numColumnas; i++) {
-				table1.addColumn(rsmd.getColumnName(i+1));
+				table1.addColumn(rsmd.getColumnName(i + 1));
 			}
 			while (rset.next()) {
 				for (int col = 1; col <= numColumnas; col++) {
@@ -278,9 +275,9 @@ public class Modelo {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public void cargarTablaUsuario() {
 		table2 = new DefaultTableModel();
 		int numColumnas = getNumColumnas(sqlTablaUsuario);
@@ -291,7 +288,7 @@ public class Modelo {
 			ResultSet rset = pstmt.executeQuery();
 			ResultSetMetaData rsmd = rset.getMetaData();
 			for (int i = 0; i < numColumnas; i++) {
-				table2.addColumn(rsmd.getColumnName(i+1));
+				table2.addColumn(rsmd.getColumnName(i + 1));
 			}
 			while (rset.next()) {
 				for (int col = 1; col <= numColumnas; col++) {
@@ -302,10 +299,9 @@ public class Modelo {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
+
 	private int getNumColumnas(String sql) {
 		int num = 0;
 		try {
@@ -335,30 +331,56 @@ public class Modelo {
 	public DefaultTableModel getTabla1() {
 		return table1;
 	}
-	
+
 	public DefaultTableModel getTabla2() {
 		return table2;
 	}
-	
+
 	public void guardarTablaEventos() {
 		File rutaTablaEV = new File(System.getProperty("user.dir"));
 		JFileChooser file = new JFileChooser(rutaTablaEV);
 		int selecionados = file.showSaveDialog(events2.getScrollPane());
-		if(selecionados == JFileChooser.APPROVE_OPTION){
-			File fch = file.getSelectedFile();			
+		if (selecionados == JFileChooser.APPROVE_OPTION) {
+			File fch = file.getSelectedFile();
 		}
 		try {
-			
+
 			FileOutputStream foutput = new FileOutputStream(fichero);
 			ObjectOutputStream ooutput = new ObjectOutputStream(foutput);
 			expTab Tabla = new expTab(table1);
 			ooutput.writeObject(Tabla);
 			foutput.close();
 			ooutput.close();
-			
+
 		} catch (IOException e) {
-			
+
 		}
 	}
+
+	public void cargarTabla() {
+			File rutaTablaEV = new File(System.getProperty("user.dir"));
+			JFileChooser file = new JFileChooser(rutaTablaEV);
+			int selecionados = file.showSaveDialog(events2.getScrollPane());
+			if(selecionados == JFileChooser.APPROVE_OPTION){
+				
+			try {
+				
+				File fich = file.getSelectedFile();
+				FileInputStream finput = new FileInputStream(fich);
+				ObjectInputStream fout = new ObjectInputStream(finput);
+				expTab Tabla = (expTab) fout.readObject();
+				
+				actualizarTab((DefaultTableModel) Tabla.getTabla());
+				
+			} catch(Exception e) {
+				
+			}
+			}
+	}
+	public void actualizarTab(DefaultTableModel tabla) {
+		this.table1 = tabla;
+		
+	}
+	
 	
 }
